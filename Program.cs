0f945b0de2d6cb6d4aacb6fe6c425768e3b26a22
Quang Thanh <quangthanh@example.com>
2025-08-06 14:35:46 +0700

    v9

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.WebHost.UseUrls("http://0.0.0.0:80"); // Đảm bảo dòng này có mặt khi đóng 
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
